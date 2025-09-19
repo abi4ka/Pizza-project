0b5fde8d2ad8971b1ac0bfa300cfa125cefc1026
abi4ka <nikita161787@gmail.com>
2025-09-19 13:48:18 +0200

    save result

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,17 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        //ArrayList<Client> list = new ArrayList<>();
         Map<String, Integer> globalLike = new HashMap<>();
         Map<String, Integer> globalNoLike = new HashMap<>();
 
         Map<String, Integer> result = new HashMap<>();
 
-        ArrayList<Client> list = parseClientsFromFile("src\\main\\resources\\text2.txt");
+        ArrayList<Client> list = parseClientsFromFile("src\\main\\resources\\text1.txt");
 
         for (Client c : list) {
             for (String s : c.like) {
@@ -22,15 +20,6 @@ public class Main {
             for (String s : c.noLike) {
                 globalNoLike.put(s, globalNoLike.getOrDefault(s, 0) + 1);
             }
-        }
-
-        System.out.println("--- Like ---");
-        for (String s : globalLike.keySet()) {
-            System.out.println("Like: " + s + " " + globalLike.get(s));
-        }
-        System.out.println("--- No Like ---");
-        for (String s : globalNoLike.keySet()) {
-            System.out.println("NoLike: " + s + " " + globalNoLike.get(s));
         }
 
         for (String ingredient : globalLike.keySet()) {
@@ -45,6 +34,8 @@ public class Main {
         for (String res : result.keySet()) {
             System.out.println("Result: " + res + " " + result.get(res));
         }
+
+        saveResultToFile(result, "src\\main\\resources\\BestPizza.txt");
     }
 
     public static ArrayList<Client> parseClientsFromFile(String filePath) {
@@ -82,5 +73,26 @@ public class Main {
             e.printStackTrace();
         }
         return clients;
+    }
+
+    public static void saveResultToFile(Map<String, Integer> result, String filePath) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            Set<String> ingredients = result.keySet();
+
+            bw.write(String.valueOf(ingredients.size()));
+            bw.write(" ");
+
+            boolean first = true;
+            for (String ing : ingredients) {
+                if (!first) bw.write(" ");
+                bw.write(ing);
+                first = false;
+            }
+            bw.newLine();
+
+            System.out.println("Result saved to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
